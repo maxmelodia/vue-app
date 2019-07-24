b1b4f7e21f20585ee0148534b523013fe0c6b8ea
Max1 <Max1@PRO-MAX.softrom.local>
2019-07-24 16:40:31 -0300

    quinto

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,16 +1,16 @@
 <template>
   <nav class="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-
-      <!--<router-link to="/" class="navbar-item logo">Code Snips</router-link>-->
-
-      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    <div id="navbarExampleTransparentExample" class="navbar-menu is-active">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <router-link :to="{ name: 'Home' }" class="navbar-item">início</router-link>
+          <router-link :to="{ name: 'BlogHome' }" class="navbar-item">blog</router-link>
+        </div>
+      </div>        
     </div>
-
+  </nav>
+    <!--  
+  <nav class="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
     <div id="navbarExampleTransparentExample" class="navbar-menu">
       <div class="navbar-start">
         <div class="navbar-item">
@@ -20,10 +20,17 @@
       </div>        
     </div>
   </nav>
+    -->
 </template>
 
 <style scoped lang="scss">
 .navbar-item {
   text-transform: lowercase;
+  padding: 5px;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
 }
 </style>
